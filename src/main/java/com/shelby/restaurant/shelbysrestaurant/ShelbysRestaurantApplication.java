e8ad4627f0f2900ad7f2b6828d75849731e612c6
Zhadan Artem <artem01zh@gmail.com>
2021-12-25 19:19:00 +0200

    initial commit

---
+++ src/main/java/com/shelby/restaurant/shelbysrestaurant/ShelbysRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.shelby.restaurant.shelbysrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelbysRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelbysRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shelby/restaurant/shelbysrestaurant/ShelbysRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelby.restaurant.shelbysrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelbysRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
